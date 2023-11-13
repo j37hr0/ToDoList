8b7f498d10c46679338e267828f38d521bb9ce70
jethro <jethro.cotton3@gmail.com>
2023-11-13 11:07:15 +0200

    added and tested sql queries for: edit entries, update archive status, insert entry

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import sql
+from sql import Connection
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox, QInputDialog
+import sys
 
 
+dbconn = sql.Connection()
+#dbconn.insert_entry("some new entry")
+dbconn.update_archive_status(2)
--- sql.py
+++ sql.py
@@ -8,10 +8,10 @@ password = config('PASSWORD')
 
 class Connection:
     def __init__(self):
-        self.conn = mysql.connector.connect(host, user, password)
+        self.conn = mysql.connector.connect(host=host, user=user, password=password)
         self.cursor = self.conn.cursor()
         self.create_database()
-        self.create_tables()
+        self.create_table()
 
     def create_database(self):
         self.cursor.execute("""
@@ -20,7 +20,7 @@ class Connection:
 
     def create_table(self):
         self.cursor.execute("""
-        CREATE TABLE `todolist`.`items` (
+        CREATE TABLE IF NOT EXISTS `todolist`.`items` (
         `id` INT NOT NULL AUTO_INCREMENT,
         `itemname` VARCHAR(45) NULL,
         `dateadded` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
@@ -33,3 +33,31 @@ class Connection:
         self.cursor.execute(f"""
         INSERT INTO `todolist`.`items`(itemname) VALUES ('{desc}')
         """)
+        self.conn.commit()
+
+    def retrieve_non_archived(self):
+        self.cursor.execute("""
+        SELECT * FROM `todolist`.`items` WHERE archived = 0;
+        """)
+
+    def retrieve_all(self):
+        self.cursor.execute("""
+        SELECT * FROM `todolist`.`items`;
+        """)
+
+    def update_archive_status(self, entryid):
+        self.cursor.execute(f"""
+        UPDATE `todolist`.`items`
+        SET archived = NOT archived
+        WHERE id = {entryid}
+        """)
+        self.conn.commit()
+
+    def edit_entry(self, entryid, entrytext):
+        self.cursor.execute(f"""
+        UPDATE `todolist`.`items`
+        SET itemname = {entrytext}
+        WHERE id = {entryid}
+        """)
+        self.conn.commit()
+
