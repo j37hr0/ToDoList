23df932b674a3cfaba039a57a3caf62bf4b24b61
jethro <jethro.cotton3@gmail.com>
2023-11-13 10:50:23 +0200

    more work on sql file, table and db creation, plus entry insert queries done

--- sql.py
+++ sql.py
@@ -10,8 +10,26 @@ class Connection:
     def __init__(self):
         self.conn = mysql.connector.connect(host, user, password)
         self.cursor = self.conn.cursor()
+        self.create_database()
+        self.create_tables()
 
-    def create_tables(self):
+    def create_database(self):
         self.cursor.execute("""
-        CREATE TABLE IF NOT EXIST 
+        CREATE DATABASE IF NOT EXISTS todolist;
+        """)
+
+    def create_table(self):
+        self.cursor.execute("""
+        CREATE TABLE `todolist`.`items` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `itemname` VARCHAR(45) NULL,
+        `dateadded` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+        `currentstatus` TINYINT DEFAULT 1,
+        `archived` TINYINT DEFAULT 0,
+        PRIMARY KEY (`id`));
+        """)
+
+    def insert_entry(self, desc):
+        self.cursor.execute(f"""
+        INSERT INTO `todolist`.`items`(itemname) VALUES ('{desc}')
         """)
