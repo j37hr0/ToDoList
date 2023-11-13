f49d69195d3748f54be73fdbe752dcdbc58d1e53
jethro <jethro.cotton3@gmail.com>
2023-11-13 10:24:37 +0200

    started making sql file

--- sql.py
+++ sql.py
@@ -1,2 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
+from decouple import config
+host = config('HOST')
+user = config('USER')
+password = config('PASSWORD')
+
+
+class Connection:
+    def __init__(self):
+        self.conn = mysql.connector.connect(host, user, password)
+        self.cursor = self.conn.cursor()
+
+    def create_tables(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXIST 
+        """)
